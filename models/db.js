7ddb4a145d25d0c2da87d2a3103e7c9c5f9c4747
waiyanmoeoo <waiyanmoeoo81@gmail.com>
2022-06-03 23:26:56 +0630

    upload project folder

---
+++ models/db.js
@@ -0,0 +1,7 @@
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/EmployeeDB',{useNewUrlParser: true,useUnifiedTopology: true},{ useFindAndModify: false },(err)=>{
+    if(!err) {console.log('MongoDB Connection Succeeded')}
+    else { console.log('Error in DB connection :' + err)}
+});
+require('./employee.model');
